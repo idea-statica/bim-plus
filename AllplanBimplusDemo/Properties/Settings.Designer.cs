d13c987cb1da1cb2ffb6b8dd8ec4f9f8123d98b9
Martin Pospisil <martin.pospisil@ideastatica.com>
2021-09-22 10:47:21 +0200

    IdeaStatiCa.Plugin 21.1.0.2883

--- AllplanBimplusDemo/Properties/Settings.Designer.cs
+++ AllplanBimplusDemo/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AllplanBimplusDemo.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace AllplanBimplusDemo.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\IDEA StatiCa\\StatiCa 20.0\\IdeaCodeCheck.exe")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\IDEA StatiCa\\StatiCa 21.1\\IdeaCheckbot.exe")]
         public string IdeaStatiCaCCM {
             get {
                 return ((string)(this["IdeaStatiCaCCM"]));
